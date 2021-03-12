5fce85786c330b6f16799c3bfb3022fd360d8298
hung-chou <hungchou.tai@zegal.com>
2021-03-12 14:40:26 +0800

    debugging

--- bin/create-sa.js
+++ bin/create-sa.js
@@ -8,9 +8,11 @@ const fs = require('fs');
 async function test() {
     try {
         const zegal = new Zegal('pk_4d457898-15b5-430a-aff9-18e8e460e454');
+        console.log("initializing");
         await zegal.init();
         let payload = fs.readFileSync(FILE);
         payload = JSON.parse(payload);
+        console.log("create document");
 
         let doc = await zegal.createDocument(payload);
         console.log("doc created: ", JSON.stringify(doc, null, 2))
